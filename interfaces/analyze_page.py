cf3760246305d5611bc88007f3d83928f1ca51b3
aboub <aboubekre.sayoud@gmail.com>
2023-11-25 14:08:39 +0100

    WIP

--- interfaces/analyze_page.py
+++ interfaces/analyze_page.py
@@ -1,5 +1,11 @@
 import tkinter as tk
+import argparse
 from tkinter import ttk
+import app.log_filter as lf
+import app.log_parser as lp
+import app.log_reader as lr
+import app.log_manager as lm
+import app.log_analyzer as la
 
 class AnalyzePage(ttk.Frame):
     def __init__(self, master):
@@ -15,10 +21,40 @@ class AnalyzePage(ttk.Frame):
         self.analyze_logs()
 
     def analyze_logs(self):
-        # Your log analysis logic here
-        # For now, let's just display the log file path
         log_text = f"Analyzing logs from: {self.log_file_path}"
-        self.log_display.insert(tk.END, log_text)
+        log_line_pattern = r'(\w+ +\d+ \d+:\d+:\d+) (\S+) (\S+): (.+)'
+
+        parser = argparse.ArgumentParser(description='Parse and filter logs.')
+
+        parser.add_argument('--log_file', default='/var/log/syslog', help='Path to the log file')
+
+        parser.add_argument('--severity', nargs='+', default=[], help='Specify severity levels to display')
+
+        parser.add_argument('--process', default='', help='Specify process name to filter logs')
+
+        args = parser.parse_args()
+
+        log_file_path = args.log_file
+        severity_levels = args.severity
+        process_name = args.process
+        log_parser = lp.LogParser(log_line_pattern)
+        log_analyzer = la.LogAnalyzer()
+        log_manager = lm.LogManager()
+        log_reader = lr.LogReader()
+        log_filter = lf.LogFilter()
+        parsed_logs = log_reader.read_log_file(log_file_path, log_parser)
+
+
+        if severity_levels or process_name:
+            filtered_logs = log_filter.filter_logs(parsed_logs, severity_levels, process_name)
+            log_manager.display_logs(filtered_logs)
+            log_text += "\n".join(filtered_logs)
+        else:
+            log_manager.display_logs(parsed_logs)
+            log_text += "\n".join(parsed_logs)
+            
+        print(log_text)
+        self.log_display.insert(tk.END, parsed_logs)
 
     def create_widgets(self):
         label = ttk.Label(self, text="Log Analysis Page")
--- interfaces/another_page.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-class AnotherPage(tk.Frame):
-    # Définissez une autre classe de page de manière similaire si nécessaire
-    pass
-
